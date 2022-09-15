a26eea2649ba611ccebaec4ffb03de9ddb15c1a7
OSFR34 <selma-ozturk@outlook.com>
2022-09-15 18:47:16 +0200

    ajout des messages d'erreur pendant l'identification et le changement d'identité

--- frontend/src/components/LoginPage/LoginPage.js
+++ frontend/src/components/LoginPage/LoginPage.js
@@ -1,83 +1,124 @@
-import React,{useState} from 'react'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-import Actions from '../../redux/actions'
-import {useDispatch, useSelector} from 'react-redux'
+import Actions from "../../redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import "./loginpage.css";
 
 export default function LoginPage() {
+  // Identification du nom
+  const [userNameState, setUserName] = useState(false);
+  // Mot de passe
+  const [passwordState, setPasswordState] = useState(false);
+  // Remember
+  const [rememberState, setRememberState] = useState(false);
+  // Error message identification
+  const [errorState, setErrorState] = useState(false);
 
-    // Identification du nom
-    const [userNameState, setUserName] = useState(false)
-    // Mot de passe
-    const [passwordState, setPasswordState] = useState(false)
-    // Remember
-    const [rememberState, setRememberState] = useState(false)
+  const isAuth = useSelector((state) => state.loginReducer);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const isAuth = useSelector(state => state.loginReducer)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const onUsernameChange = (e) => {
+    let userNameValue = e.target.value;
+    setUserName(userNameValue);
+  };
 
-    const onUsernameChange = (e) => {
-        let userNameValue = e.target.value;
-        setUserName(userNameValue)
+  const onPasswordChange = (e) => {
+    let userPasswordValue = e.target.value;
+    setPasswordState(userPasswordValue);
+    console.log(userPasswordValue);
+  };
+
+  const onRememberChange = (e) => {
+    let userRememberValue = e.target.checked;
+    setRememberState(userRememberValue);
+  };
+  const onFormSubmitFunction = (e) => {
+    e.preventDefault();
+    if (userNameState === false || passwordState === false) {
+      setErrorState(true);
+      setTimeout(function () {
+        document.querySelector(".error-message").style.display = "none";
+        setErrorState(false)
+      }, 3000);
+    } else {
+      axios
+        .post("http://localhost:3001/api/v1/user/login", {
+          email: userNameState,
+          password: passwordState,
+        })
+        .then((result) => {
+          if (result.data.status === 200) {
+            dispatch(
+              Actions.userAction.logIn({
+                loggedin: true,
+                token: result.data.body.token,
+                user: result.data.body.user,
+              })
+            );
+            navigate("/profile");
+          }
+        })
+        .catch((error) => {
+          setErrorState(true);
+          setTimeout(function () {
+            document.querySelector(".error-message").style.display = "none";
+            setErrorState(false)
+          }, 3000);
+        });
     }
+  };
 
-    const onPasswordChange = (e) => {
-        let userPasswordValue = e.target.value;
-        setPasswordState(userPasswordValue)
-        console.log(userPasswordValue)
-    }
-
-    const onRememberChange = (e) => {
-        let userRememberValue = e.target.checked;
-        setRememberState(userRememberValue)
-    }
-    const onFormSubmitFunction = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:3001/api/v1/user/login',{
-            email:userNameState,
-            password:passwordState        
-        }).then(result => {
-            if (result.data.status === 200) {
-                dispatch (Actions.userAction.logIn({
-                    loggedin:true,
-                    token:result.data.body.token,
-                    user:result.data.body.user
-                }))
-                navigate('/profile')
-            }            
-        }).catch(error => console.error())
-        
-    }
-
-    return (
-        <div>
-            <Header/>
-            <main className="main bg-dark">
-                <section className="sign-in-content">
-                    <i className="fa fa-user-circle sign-in-icon"></i>
-                    <h1>Sign In</h1>
-                    <form onSubmit={onFormSubmitFunction} >
-                        <div className="input-wrapper">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" id="username" onChange={onUsernameChange} />
-                        </div>
-                        <div className="input-wrapper">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" id="password" onChange={onPasswordChange}  />
-                        </div>
-                        <div className="input-remember">
-                            <input type="checkbox" id="remember-me" onChange={onRememberChange}/>
-                            <label htmlFor="remember-me">Remember me</label>
-                        </div>
-                        <button type="submit" className="sign-in-button" style={{cursor:'pointer' }} >Sign In</button>                
-                    </form>
-                </section>
-                </main>
-            <Footer/>      
-        </div>
-  )
+  return (
+    <div>
+      <Header />
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          {errorState ? (
+            <div className="error-message">
+              Signin failed
+              <br />
+              You must cleck your usename and password.
+            </div>
+          ) : null}
+          <form onSubmit={onFormSubmitFunction}>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" onChange={onUsernameChange} />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                onChange={onPasswordChange}
+              />
+            </div>
+            <div className="input-remember">
+              <input
+                type="checkbox"
+                id="remember-me"
+                onChange={onRememberChange}
+              />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            <button
+              type="submit"
+              className="sign-in-button"
+              style={{ cursor: "pointer" }}
+            >
+              Sign In
+            </button>
+          </form>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
 }
--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -1,137 +1,186 @@
-import React, { useState } from 'react' 
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import axios from 'axios'
-import './profilepage.css'
-import Actions from '../../redux/actions'
+import React, { useState } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import "./profilepage.css";
+import Actions from "../../redux/actions";
 
 export default function ProfilePage() {
+  const isAuth = useSelector((state) => state.loginReducer);
+  const dispatch = useDispatch();
+  const [editingOpen, setEditingOpen] = useState(false);
+  const [firstNameState, setFirstNameState] = useState(isAuth.user.firstName);
+  const [lastNameState, setLastNameState] = useState(isAuth.user.lastName);
+  const [errorState, setErrorState] = useState(false);
 
-    const isAuth = useSelector(state => state.loginReducer)
-    const dispatch = useDispatch()
-    const [editingOpen, setEditingOpen] = useState(false)
-    const [firstNameState,setFirstNameState] = useState(isAuth.user.firstName)
-    const [lastNameState,setLastNameState] = useState(isAuth.user.lastName)
-    const navigate = useNavigate()
-    
-    const handleEditOpen = () => {
-        setEditingOpen(true)
-    }
-    const handleEditClose = () => {
-        setEditingOpen(false)
-    }
+  const navigate = useNavigate();
 
-    const onFirstNameChange = () => {
-        const newFirstName = document.querySelector('#firstname').value
-        setFirstNameState(newFirstName)
-    }
-    const onLastNameChange = () => {
-        const newLastName = document.querySelector('#lastname').value
-        setLastNameState(newLastName)
-    }
+  const handleEditOpen = () => {
+    setEditingOpen(true);
+  };
+  const handleEditClose = () => {
+    setEditingOpen(false);
+  };
 
-    const handleSubmit = () => {
-        let firstNameValue = firstNameState
-        let lastNameValue = lastNameState
-        const headers = {
-            'Content-Type':'application/json',
-            'Authorization':`Bearer ${isAuth.token}`
-        }
-        const body = {
-            firstName:firstNameValue,
-            lastName:lastNameValue
-        }
-        axios.put('http://localhost:3001/api/v1/user/profile',body,{
-            headers:headers
+  const onFirstNameChange = () => {
+    const newFirstName = document.querySelector("#firstname").value;
+    setFirstNameState(newFirstName);
+  };
+  const onLastNameChange = () => {
+    const newLastName = document.querySelector("#lastname").value;
+    setLastNameState(newLastName);
+  };
+
+  const handleSubmit = () => {
+    let firstNameValue = firstNameState;
+    let lastNameValue = lastNameState;
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${isAuth.token}`,
+    };
+    const body = {
+      firstName: firstNameValue,
+      lastName: lastNameValue,
+    };
+    if (firstNameValue === "" || lastNameValue === "") {
+      setErrorState(true);
+      setTimeout(function () {
+        document.querySelector(".error-message1").style.display = "none";
+        setErrorState(false);
+      }, 3000);
+    } else {
+      axios
+        .put("http://localhost:3001/api/v1/user/profile", body, {
+          headers: headers,
         })
-        .then(result => {
-            if(result.status === 200){
-                axios.post('http://localhost:3001/api/v1/user/profile',[],{
-                    headers:headers
-                }).then(result => {
-                    dispatch(Actions.userAction.logIn({
-                        loggedin:true,
-                        token:isAuth.token,
-                        user:result.data.body
-                    }))
-                    navigate('/profile')
-
-                }).catch(error => console.error(error))
-                setEditingOpen(false)         
-            }
-            
+        .then((result) => {
+          if (result.status === 200) {
+            axios
+              .post("http://localhost:3001/api/v1/user/profile", [], {
+                headers: headers,
+              })
+              .then((result) => {
+                dispatch(
+                  Actions.userAction.logIn({
+                    loggedin: true,
+                    token: isAuth.token,
+                    user: result.data.body,
+                  })
+                );
+                navigate("/profile");
+              })
+              .catch((error) => console.error(error));
+            setEditingOpen(false);
+          }
         })
-        .catch(error => console.error(error))
-
+        .catch((error) => console.error(error));
     }
-    // Redirection vers la page Detail
-    const handleNavigateDetail = (e, id) => {
-       navigate(`/transactions/${id}`)
-    }
+  };
 
-    return (
-        <div>
-            <Header />
-            <main className="main bg-dark">
-                {
-                    editingOpen ? <div className='header header-open'>
-                        <h1>Welcome Back</h1>
-                        <div className='header-open-content'>
-                            <div className='input-group'>
-                                <input id="firstname" placeholder={isAuth.user.firstName} onChange={onFirstNameChange}  />
-                                <input id="lastname" placeholder={isAuth.user.lastName} onChange={onLastNameChange}  />
-                            </div>
-                            <div className='button-group'>
-                                <button className='save-button' onClick={handleSubmit}>Save</button>
-                                <button className='cancel-button' onClick={handleEditClose}>Cancel</button>
-                            </div>
-                        </div>
-                    </div> : <div className="header">
+  const handleNavigateDetail = (e, id) => {
+    navigate(`/transactions/${id}`);
+  };
 
-                        <h1>Welcome Back<br />
-                            {
-                                isAuth.user.firstName + ' ' + isAuth.user.lastName +' !'
-                            }
-                        </h1>
-                        <button className="edit-button" onClick={handleEditOpen}>Edit Name</button>
-                    </div>
-                }
+  return (
+    <div>
+      <Header />
+      <main className="main bg-dark">
+        {editingOpen ? (
+          <div className="header header-open">
+            <h1>Welcome Back</h1>
+            {errorState ? (
+              <div className="error-message1">
+                Name or Surname can't be empty!
+              </div>
+            ) : null}
+            <div className="header-open-content">
+              <div className="input-group">
+                <input
+                  id="firstname"
+                  placeholder={isAuth.user.firstName}
+                  onChange={onFirstNameChange}
+                />
+                <input
+                  id="lastname"
+                  placeholder={isAuth.user.lastName}
+                  onChange={onLastNameChange}
+                />
+              </div>
+              <div className="button-group">
+                <button className="save-button" onClick={handleSubmit}>
+                  Save
+                </button>
+                <button className="cancel-button" onClick={handleEditClose}>
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="header">
+            <h1>
+              Welcome Back
+              <br />
+              {isAuth.user.firstName
+                ? isAuth.user.firstName + " " + isAuth.user.lastName + "!"
+                : null}
+            </h1>
+            <button className="edit-button" onClick={handleEditOpen}>
+              Edit Name
+            </button>
+          </div>
+        )}
 
-                <h2 className="sr-only">Accounts</h2>
-                <section className="account">
-                    <div className="account-content-wrapper">
-                        <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                        <p className="account-amount">$2,082.79</p>
-                        <p className="account-amount-description">Available Balance</p>
-                    </div>
-                    <div className="account-content-wrapper cta">
-                        <button className="transaction-button" onClick={(e) => handleNavigateDetail(e,1)}>View transactions</button>
-                    </div>
-                </section>
-                <section className="account">
-                    <div className="account-content-wrapper">
-                        <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                        <p className="account-amount">$10,928.42</p>
-                        <p className="account-amount-description">Available Balance</p>
-                    </div>
-                    <div className="account-content-wrapper cta">
-                        <button className="transaction-button" onClick={(e) => handleNavigateDetail(e,2)}>View transactions</button>
-                    </div>
-                </section>
-                <section className="account">
-                    <div className="account-content-wrapper">
-                        <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-                        <p className="account-amount">$184.30</p>
-                        <p className="account-amount-description">Current Balance</p>
-                    </div>
-                    <div className="account-content-wrapper cta">
-                        <button className="transaction-button" onClick={(e) => handleNavigateDetail(e,3)}>View transactions</button>
-                    </div>
-                </section>
-            </main>
-            <Footer />
-        </div>
-    )
+        <h2 className="sr-only">Accounts</h2>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+            <p className="account-amount">$2,082.79</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button
+              className="transaction-button"
+              onClick={(e) => handleNavigateDetail(e, 1)}
+            >
+              View transactions
+            </button>
+          </div>
+        </section>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+            <p className="account-amount">$10,928.42</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button
+              className="transaction-button"
+              onClick={(e) => handleNavigateDetail(e, 2)}
+            >
+              View transactions
+            </button>
+          </div>
+        </section>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+            <p className="account-amount">$184.30</p>
+            <p className="account-amount-description">Current Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button
+              className="transaction-button"
+              onClick={(e) => handleNavigateDetail(e, 3)}
+            >
+              View transactions
+            </button>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
 }
